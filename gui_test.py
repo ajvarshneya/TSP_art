23aaa4aed28ed1c16fa24da71070d559cbe25e35
sdgennari <sdgennari@gmail.com>
2015-04-25 01:04:14 -0400

    Added method to find MST

--- gui_test.py
+++ gui_test.py
@@ -3,56 +3,60 @@ from data_gen import get_point_list
 import random
 
 def draw_content(c, width, height):
-    r = 1
     points = get_point_list()
-    #random.shuffle(points)
+    random.shuffle(points)
 
     edge_list = generate_edge_list(points)
 
-    mst = find_MST(edge_list, len(points))
+    mst_edges = find_MST_edges(edge_list, len(points))
 
+    r = 1
     for i in range(len(points)-1):
         x_c = points[i][0]-800
         y_c = points[i][1]
         c.create_oval(x_c-r, y_c-r, x_c+r, y_c+r, fill="#000000")
-        #c.create_line(x_c, y_c, points[i+1][0]-800, points[i+1][1])
 
-    print(mst[0])
-    print(mst[0][1][0])
-
-    for i in range(len(mst)):
-        c.create_line(mst[i][1][0]-800, mst[i][1][1], mst[i][2][0]-800, mst[i][2][1])
+    for i in range(len(mst_edges)):
+        c.create_line(mst_edges[i][1][0]-800, mst_edges[i][1][1], mst_edges[i][2][0]-800, mst_edges[i][2][1])
 
 def generate_edge_list(point_list):
     edge_list = []
     # Generate the list of all possible edges
-    print("Generating edge list...", len(point_list))
     for i in range(len(point_list)):
         # Avoid duplicates since edges are symmetric
         for j in range(i+1, len(point_list)):
             weight = calc_square_distance(point_list[i], point_list[j])
             edge = (weight, point_list[i], point_list[j])
             edge_list.append(edge)
-    print("Edge list complete", len(edge_list))
 
     return edge_list
 
-def find_MST(edge_list, num_nodes):
+def find_MST_edges(edge_list, num_nodes):
     # Sort the list by the first value (distance)
     edge_list.sort(key=lambda x:x[0])
 
-    # Use dictionary to have O(1) lookups on average
+    mst_edges = []
+
+    # Use dictionary for O(1) lookups
     visited = {}
-    MST_edges = []
-    for i in range(len(edge_list)):
-        if edge_list[i][1] not in visited or edge_list[i][2] not in visited:
-            MST_edges.append(edge_list[i])
-            visited[edge_list[i][1]] = True
-            visited[edge_list[i][2]] = True
 
-    print(len(MST_edges))
+    # Mark first node as read
+    visited[edge_list[0][1]] = True
 
-    return MST_edges
+    # Continue until all nodes have been visited
+    while len(visited) != num_nodes:
+        # Iterate through all edges
+        for i in range(len(edge_list)):
+            u = edge_list[i][1]
+            v = edge_list[i][2]
+
+            if (u in visited and v not in visited) or (u not in visited and v in visited):
+                visited[u] = True
+                visited[v] = True
+                mst_edges.append(edge_list[i])
+                break
+
+    return mst_edges
 
 def calc_square_distance(p1, p2):
     dx = p1[0]-p2[0]
